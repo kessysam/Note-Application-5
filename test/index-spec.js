0413bdf624558a6a509b436112cefb7b74a90e76
kessysam <kessysam@gmail.com>
2016-08-18 19:10:44 +0100

    feature

--- test/index-spec.js
+++ test/index-spec.js
@@ -3,9 +3,31 @@ var noteApp = require("../index");
 var assert = chai.assert;
 
 describe("This is a test function,", function (author){
+
+	var note = new noteApp();
+	note.create("lkjdfglkjs");
+
+	console.log(note);
 	it(" should assign value to author", function() {
-		var note = new noteApp();
-		//assert.equal('Unknown Author', note.author );
 		assert.equal(note.author, 'anonymous');
+	});
+});
+
+describe("This test the create() of the created object created. ", function(author){
+
+	var note = new noteApp();
+
+	it(" checks if the argument passed in, is at the intended index. ", function() {
+		note.create("lsfdjbvklsjdf");
+		assert(note.notes[note.notes.length - 1], "lsfdjbvklsjdf");
+		//console.log(note);
+		//assert.equal(note.notes.length, 2);
+	});
+
+	it(" checks if the argument passed in, is at the intended index. ", function() {
+		//note.create("lsfdjbvklsjdf");
+		//assert(note.notes[note.notes.length - 1], "lsfdjbvklsjdf");
+		console.log(note);
+		assert.equal(note.notes.length, 1);
 	});
 });
